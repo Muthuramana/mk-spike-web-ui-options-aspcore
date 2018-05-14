2f74f8eca70633ea7da89d61b76722a64979c69d
Muthu <muthu@lightningcloudconsultancy.com>
2018-05-15 00:25:34 +0100

    sleep

--- Scripts/1-Bringup-Frontend.ps1
+++ Scripts/1-Bringup-Frontend.ps1
@@ -2,4 +2,5 @@ $currentloc = [string](Get-Location)
 $parentloc = (Get-Item $currentloc).Parent.Parent.Parent.FullName
 Set-Location $parentloc\Frontend\mk-spike-web-ui-options-aspcore\Careers.Freshlook
 docker-compose -p dfcspike up -d
+Start-Sleep 5
 & "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:5000
