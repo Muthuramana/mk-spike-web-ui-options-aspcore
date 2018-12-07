1412f75e0cbeeb7bc6573822eba5221c07bb8f79
Muthu <muthu@lightningcloudconsultancy.com>
2018-12-07 16:54:29 +0000

    update

--- Scripts/3-Skills-service-v1.ps1
+++ Scripts/3-Skills-service-v1.ps1
@@ -1,10 +1,12 @@
 $currentloc = [string](Get-Location)
 try
 {
+    docker stop dfcspike_dfc.microservices.skills_1
+
     $parentloc = (Get-Item $currentloc).Parent.Parent.Parent.FullName
 
     Set-Location $parentloc\SkillsContentService\DFCSkillsMicroservice\DFC.Microservices.Skills
-    docker-compose -p dfcspike up -d
+    docker-compose -p dfcspike up -d --build
 
     Set-Location $parentloc\SkillsRepo\dfc.skills.data\dfc.skills.repo.docker
     docker-compose -p dfcspike up -d
